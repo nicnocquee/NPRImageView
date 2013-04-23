97ab249573205fab8386d92b8b7a14598bc9d7d2
Nico Prananta <nicnocquee@yahoo.com>
2013-04-23 22:54:00 +0900

    show error message when image fail downloaded.

--- Class/NPRImageView.m
+++ Class/NPRImageView.m
@@ -352,6 +352,8 @@
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self.messageLabel setText:NSLocalizedString(@"Image cannot be downloaded", nil)];
                 [self.messageLabel setHidden:NO];
+                [self.indicatorView stopAnimating];
+                [self setNeedsLayout];
             });
             
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
