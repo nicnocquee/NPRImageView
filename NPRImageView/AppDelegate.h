12c6459fe7bd1176323a4826b14b3dbce2cf619e
Nico Prananta <nicnocquee@yahoo.com>
2013-04-23 18:15:22 +0900

    Initial Commit

---
+++ NPRImageView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NPRImageView
+//
+//  Created by Nico Prananta on 4/23/13.
+//  Copyright (c) 2013 Touches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NPRImageView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NPRImageView
+//
+//  Created by Nico Prananta on 4/23/13.
+//  Copyright (c) 2013 Touches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
