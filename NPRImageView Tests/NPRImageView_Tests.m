92bf4add954e08fb3a6f157543384fff87d6748b
Nico Prananta <nicnocquee@yahoo.com>
2013-06-17 15:41:57 +0900

    test

---
+++ NPRImageView Tests/NPRImageView_Tests.m
@@ -0,0 +1,36 @@
+//
+//  NPRImageView_Tests.m
+//  NPRImageView Tests
+//
+//  Created by Nico Prananta on 6/17/13.
+//  Copyright (c) 2013 Touches. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NPRImageView_Tests : XCTestCase
+
+@end
+
+@implementation NPRImageView_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
